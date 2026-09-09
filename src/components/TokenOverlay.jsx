6812676085f1f3aca58a4118f71742c2bb3c97d0
Shivam Kumar <heyshiivam556@gmail.com>
2026-09-09 06:25:37 +0530

    fix: remove duplicate useLiveClock in TokenOverlay causing build failure

--- src/components/TokenOverlay.jsx
+++ src/components/TokenOverlay.jsx
@@ -53,15 +53,6 @@ function useLiveClock() {
   return time;
 }
 
-function useLiveClock() {
-  const [time, setTime] = useState(new Date());
-  useEffect(() => {
-    const id = setInterval(() => setTime(new Date()), 1000);
-    return () => clearInterval(id);
-  }, []);
-  return time;
-}
-
 export default function TokenOverlay({ onClose }) {
   const { user } = useAuth();
   const time     = useLiveClock();
